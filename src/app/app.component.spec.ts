840870f4279f8ec93f412fe3af9c109d800bfc01
BR0KLYN9 <cauagran@gmail.com>
2024-03-19 17:44:50 -0300

    teste

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'calculoimc'`, () => {
+  it(`should have as title 'calculadoraimctop'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('calculoimc');
+    expect(app.title).toEqual('calculadoraimctop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, calculoimc');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, calculadoraimctop');
   });
 });
